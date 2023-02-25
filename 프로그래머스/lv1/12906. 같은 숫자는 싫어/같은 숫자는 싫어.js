28af742bb734886120b11330aba39d86a3b07c15
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-02-25 22:24:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 57.13 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,8 +1,1 @@
-const solution = (arr) => {
-    let r = [arr[0]]
-    for (let i = 0; i < arr.length - 1; i++){
-        if (arr[i] !== arr[i + 1])
-            r.push(arr[i + 1])
-    }
-    return r
-}
+const solution = (arr) => arr.filter((a, i) => a !== arr[i + 1])
