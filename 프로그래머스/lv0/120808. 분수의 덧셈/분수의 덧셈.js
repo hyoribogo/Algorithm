03fd9011bf93008b8f36dd97cd1607a88d98085e
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-02-26 21:17:04 +0900

    [level 0] Title: 분수의 덧셈, Time: 2.50 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,32 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let result_de=0;
+    let result_nu=0;
+    if (denom1===denom2){
+        result_nu=numer1+numer2;
+        result_de=denom1
+    }
+    else {
+        result_nu=numer1*denom2+numer2*denom1;
+        result_de=denom1*denom2;
+    }
+    return div(result_nu, result_de);
+}
+
+function div(n1, n2) {
+    if (n1===1 || n2===1){
+        console.log(n1,n2);
+        return [n1, n2];
+    }
+        
+    for (let i=2; i<=Math.min(n1,n2); i++){
+        if(n1%i===0 && n2%i===0) {
+            n1/=i;
+            n2/=i;
+            console.log(n1,n2);
+            [n1, n2] = div(n1, n2);
+            break;
+        }
+    }
+    console.log(n1,n2);
+    return [n1, n2];
+}
