c6e291b968d3389d000251bf8f3952135c22456e
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-02-26 23:58:17 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,29 +1,10 @@
 function solution(numer1, denom1, numer2, denom2) {
-    let result_de=0;
-    let result_nu=0;
-    if (denom1===denom2){
-        result_nu=numer1+numer2;
-        result_de=denom1
-    }
-    else {
-        result_nu=numer1*denom2+numer2*denom1;
-        result_de=denom1*denom2;
-    }
-    return div(result_nu, result_de);
+    let num = numer1 * denom2 + numer2 * denom1
+    let den = denom1 * denom2
+    let quo = gcd(num, den)
+    return [num / quo, den / quo]
 }
 
-function div(n1, n2) {
-    if (n1===1 || n2===1){
-        return [n1, n2];
-    }
-        
-    for (let i=2; i<=Math.min(n1,n2); i++){
-        if(n1%i===0 && n2%i===0) {
-            n1/=i;
-            n2/=i;
-            [n1, n2] = div(n1, n2);
-            break;
-        }
-    }
-    return [n1, n2];
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a%b)
 }
