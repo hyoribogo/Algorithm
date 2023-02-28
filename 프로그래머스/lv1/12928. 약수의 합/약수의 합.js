0c818c0de5e0d8a5779e76090c9d9aa18df78fc3
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-02-28 22:18:15 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,13 +1,12 @@
 function solution(n) {
     let sum = 0
     for (let i=1; i<=Math.sqrt(n); i++){
-        if(n % i === 0){
+        if(!(n % i)){
             if(i === Math.sqrt(n)){
                 sum += Math.sqrt(n)
                 break
             }
-            sum += i
-            sum += n/i
+            sum += (i + n/i)
         }
     }
     return sum
