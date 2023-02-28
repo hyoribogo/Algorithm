fcd7d0ff2e0a0dfd6d9f5c78a3e4a899557f95f0
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-02-28 20:51:24 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,8 +1,3 @@
 function solution(seoul) {
-    let idx
-    seoul.forEach((v, i) => {
-        if(v === "Kim")
-            idx = i
-    })
-    return `김서방은 ${idx}에 있다`
+    return `김서방은 ${seoul.findIndex(i => i === "Kim")}에 있다`
 }
