31091d885d0e7f90062d3f0e758471eb1f77a9db
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-03-02 00:13:57 +0900

    [level 0] Title: 옹알이 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,3 @@
+function solution(babbling) {
+    return babbling.join().split("aya").join(" ").split("ye").join(" ").split("woo").join(" ").split("ma").join("").split(",").map(i => i.trim()).filter(i => i === "").length
+}
