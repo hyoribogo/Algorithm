4bf740d1d632efe3bfdf0e677178fe5b8314977c
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-03-05 20:14:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,14 +1,6 @@
 function solution(arr) {
     if (arr.length === 1) return [-1]
-    let min = Number.MAX_SAFE_INTEGER
-    let idx = 0
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] < min) {
-            min = arr[i]
-            idx = i
-        }
-    }
-    arr.splice(idx, 1)
+    arr.splice(arr.indexOf(Math.min(...arr)),1);
         
     return arr;
 }
