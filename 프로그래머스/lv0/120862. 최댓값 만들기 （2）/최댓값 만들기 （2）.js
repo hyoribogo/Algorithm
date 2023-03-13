2d18737352bd13a28170083cbf70026f13bb36ce
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-03-13 20:16:49 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,5 +1,4 @@
 const solution = numbers => {
     numbers = numbers.sort((a, b) => a - b)
-    const le = numbers.length
-    return Math.max(numbers[0] * numbers[1], numbers[le - 2] * numbers[le - 1])
+    return Math.max(numbers[0] * numbers[1], numbers.at(-1) * numbers.at(-2))
 }
