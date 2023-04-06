518d04887cfa166219e163cbb11b6634871e8cdf
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-07 00:13:30 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,8 @@
+function solution(array, commands) {
+    let result = []
+    
+    for(let [i, j, k] of commands)
+        result.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1])
+    
+    return result
+}
