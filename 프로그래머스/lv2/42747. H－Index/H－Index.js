c689e6114f76c0d63801dd0bb5ebddf553175261
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-11 18:43:08 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,8 +1,1 @@
-function solution(citations) {
-    citations.sort((a, b) => b - a)
-    console.log(citations)
-    for(let i = citations[0]; i >= 0; i--) {
-        if(citations.filter(v => v >= i).length >= i)
-            return i
-    }
-}
+const solution = (citations) => citations.sort((a, b) => b - a).filter((el, idx) => el >= idx + 1).length;
