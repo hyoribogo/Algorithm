6ac26584e914326594345a836ad6ebbb871fd3bb
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-14 20:48:39 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    const arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for(let i = 0; i < 10; i++)
+        numbers = numbers.replaceAll(arr[i], i.toString())
+    return +numbers
+}
