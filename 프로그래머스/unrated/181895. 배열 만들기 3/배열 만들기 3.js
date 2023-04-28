6f12b516e29ff7535455eec373c9d57bbf68517f
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-28 20:13:02 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.44 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,6 +1,5 @@
 function solution(arr, intervals) {
-    let answer = []
-    for(let [start, end] of intervals)
-        answer.push(arr.slice(start, end+1))
-    return answer.flat()
+    let [start1, end1] = intervals[0]
+    let [start2, end2] = intervals[1]
+    return [...arr.slice(start1, end1 + 1), ...arr.slice(start2, end2 + 1)]
 }
