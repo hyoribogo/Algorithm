67f5f548f0eb1f7b7ab401f87b19891a06f816cf
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-29 03:20:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.17 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,17 @@
 function solution(participant, completion) {
-    const a = participant.sort()
-    const b = completion.sort()
-    
-    for(let i = 0; i < b.length; i++) {
-        if(a[i] !== b[i])
-            return a[i]
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
     }
-    return a.at(-1)
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    // return 'nothing';
 }
