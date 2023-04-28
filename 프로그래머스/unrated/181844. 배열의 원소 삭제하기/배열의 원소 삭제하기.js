3ae5aab8cd4dea6dd9dd5e05279d1d87c5d4c32b
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-28 19:52:21 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,11 +1,3 @@
 function solution(arr, delete_list) {
-    for(let i of delete_list) {
-        arr = arr.includes(i) ? del(arr, i) : arr
-    }
-    return arr
-}
-
-function del(arr, value) {
-    let idx = arr.indexOf(value)
-    return [...arr.slice(0, idx), ...arr.slice(idx + 1)]
+    return arr.filter(i => !delete_list.includes(i))
 }
