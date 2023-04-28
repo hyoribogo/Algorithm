5d984aa98cd6c1ce9aa4e0e82b0070f5122691d4
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-29 01:19:30 +0900

    [unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,10 @@
+function solution(ineq, eq, n, m) {
+    if(ineq+eq === "<=")
+        return n <= m ? 1 : 0
+    if(ineq+eq === "<!")
+        return n < m ? 1 : 0
+    if(ineq+eq === ">=")
+        return n >= m ? 1 : 0
+    if(ineq+eq === ">!")
+        return n > m ? 1 : 0
+}
