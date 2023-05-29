51b13b99d18261126c97a91c88790e0be48f0c1e
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-05-29 19:34:00 +0900

    [level 3] Title: 단속카메라, Time: 4.49 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
@@ -1,6 +1,5 @@
 function solution(routes) {
     routes.sort((a, b) => a[0] - b[0]).sort((a, b) => a[1] - b[1])
-    console.log(routes)
     
     let end = routes[0][1]
     let result = 1
@@ -14,10 +13,3 @@ function solution(routes) {
     
     return result
 }
-
-// 모든 차가 1번 이상 단속용 카메라 만난다.
-// routes 각각의 원소는 차량의 이동 경로 진입 지점, 나간 지점
-// 차량은 10,000대 이하
-// 진입 지점, 진출 지점 범위는 -3만~ 3만
-// 최소 몇 대의 카메라를 설치해야 하는가?
-// 첫번째 원소부터 비교하자. 
