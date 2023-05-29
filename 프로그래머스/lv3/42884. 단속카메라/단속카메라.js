78ab4f31cdded476497ecb76a920debdaf52ff97
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-05-29 19:38:38 +0900

    [level 3] Title: 단속카메라, Time: 2.76 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.js
@@ -1,12 +1,12 @@
 function solution(routes) {
-    routes.sort((a, b) => a[0] - b[0]).sort((a, b) => a[1] - b[1])
+    routes.sort((a, b) => a[1] - b[1])
     
     let end = routes[0][1]
     let result = 1
     
-    for(let i = 1; i < routes.length; i++) {
-        if(end < routes[i][0]) {
-            end = routes[i][1]
+    for(const r of routes) {
+        if(end < r[0]) {
+            end = r[1]
             result++
         }
     }
