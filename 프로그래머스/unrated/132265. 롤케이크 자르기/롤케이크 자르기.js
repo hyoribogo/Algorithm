34494dbe0f0cd33f8f718f2713e3b1d7410c7b9e
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-06-04 16:15:34 +0900

    [unrated] Title: 롤케이크 자르기, Time: 416.02 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,14 @@
+function solution(topping) {
+  const [b1Dict, b1Arr, b2Dict, b2Arr] = [new Set(), [], new Set(), []];
+  topping.forEach((t1, i) => {
+    const t2 = topping.at(-i);
+    b1Dict.add(t1);
+    if (i !== 0) b2Dict.add(t2);
+    b1Arr.push(b1Dict.size);
+    b2Arr.push(b2Dict.size);
+  });
+  return b1Arr.reduce(
+    (acc, cur, i) => (cur === b2Arr.at(-i - 1) ? acc + 1 : acc),
+    0
+  );
+}
