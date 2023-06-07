eb829110a496b7b8d58b872cbc438c70376b524f
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-06-07 16:34:56 +0900

    [level 3] Title: 베스트앨범, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,28 +1,20 @@
 function solution(genres, plays) {
-    const playMap = new Map()
-    const songs = []
-    for(let i = 0; i < genres.length; i++) {
-        playMap.set(genres[i], (playMap.get(genres[i]) || 0) + plays[i])
-        songs.push([genres[i], plays[i], i])
-    }
+    const genreMap = new Map()
     
-    songs.sort((a, b) => {
-        if(playMap.get(b[0]) !== playMap.get(a[0]))
-            return playMap.get(b[0]) - playMap.get(a[0])
-        else if(b[1] !== a[1])
-            return b[1] - a[1]
-        else
-            a[2] - b[2]
+    genres
+        .map((genre, index) => [genre, plays[index]])
+        .forEach(([genre, play], index) => {
+        const data = genreMap.get(genre) || { total: 0, songs: [] }
+        genreMap.set(genre, {
+            total: data.total + play,
+            songs: [...data.songs, { play, index }]
+                .sort((a, b) => b.play - a.play)
+                .slice(0, 2)
+        })
     })
     
-    let cnt = 0
-    let curr = songs[0][0]
-    
-    return songs.filter(song => {
-        if(curr !== song[0]) {
-            curr = song[0]
-            cnt = 0
-        }
-        return cnt++ < 2
-    }).map(i => i[2])
+    return [...genreMap.entries()]
+        .sort((a, b) => b[1].total - a[1].total)
+        .flatMap(item => item[1].songs)
+        .map(song => song.index)
 }
