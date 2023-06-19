c3939fee127578b54f08d40c681515315c67ff55
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-06-19 19:55:22 +0900

    [unrated] Title: 숫자 변환하기, Time: 26.13 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
@@ -4,8 +4,8 @@ function solution(x, y, n) {
     
     for(let i = x + 1; i < y + 1; i++) {
         if (x <= i - n) dp[i] = Math.min(dp[i], dp[i - n] + 1)
-    if (i % 2 === 0 && x <= i / 2) dp[i] = Math.min(dp[i], dp[i / 2] + 1)
-    if (i % 3 === 0 && x <= i / 3) dp[i] = Math.min(dp[i], dp[i / 3] + 1)
+        if (i % 2 === 0 && x <= i / 2) dp[i] = Math.min(dp[i], dp[i / 2] + 1)
+        if (i % 3 === 0 && x <= i / 3) dp[i] = Math.min(dp[i], dp[i / 3] + 1)
     }
     
     return dp[y] === Infinity ? -1 : dp[y]
