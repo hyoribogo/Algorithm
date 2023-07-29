1a37f1b15dcfd3995353779654e4d23a658e53b3
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-07-30 03:39:42 +0900

    [level 4] Title: 징검다리, Time: 34.08 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.js
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.js
@@ -1,36 +1,35 @@
 function solution(distance, rocks, n) {
-    rocks.sort((a, b) => a - b); // 바위 위치를 오름차순으로 정렬
-    rocks.push(distance); // 도착지점도 마지막에 추가
+    rocks.sort((a, b) => a - b)
+    rocks.push(distance)
 
-    let left = 0;
-    let right = distance;
-    let answer = 0;
+    let left = 0
+    let right = distance
+    let answer = 0
 
     while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        let prevRock = 0;
-        let removedRocks = 0;
-        let minDistance = Number.MAX_SAFE_INTEGER;
+        const mid = Math.floor((left + right) / 2)
+        let prevRock = 0
+        let removedRocks = 0
+        let minDistance = 1000000000
 
-        // 각 바위 사이의 거리를 확인하면서 최솟값을 구한다
         for (const rock of rocks) {
-            const diff = rock - prevRock;
+            const diff = rock - prevRock
             if (diff < mid) {
-                removedRocks++;
+                removedRocks++
             } else {
-                minDistance = Math.min(minDistance, diff);
-                prevRock = rock;
+                minDistance = Math.min(minDistance, diff)
+                prevRock = rock
             }
         }
 
-        // n개 이상의 바위를 제거할 수 있으면 더 큰 값을 찾기 위해 left를 늘린다
-        if (removedRocks <= n) {
-            answer = minDistance;
-            left = mid + 1;
-        } else { // n개보다 많은 바위를 제거해야 한다면 더 작은 값을 찾기 위해 right를 줄인다
-            right = mid - 1;
+        if (removedRocks > n) {
+            right = mid - 1
+            continue
         }
+        
+        answer = minDistance
+        left = mid + 1
     }
 
-    return answer;
+    return answer
 }
