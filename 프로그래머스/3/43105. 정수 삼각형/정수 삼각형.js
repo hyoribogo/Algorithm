dbc4b342f37f6cb51afdcb2aea2b3250407058ba
hyoribogo <mesmerize0416@naver.com>
2024-03-05 14:15:00 +0900

    [level 3] Title: 정수 삼각형, Time: 6.08 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,8 @@
+function solution(triangle) {
+    for (let i = 1; i < triangle.length; i++) {
+        for (let j = 0; j < triangle[i].length; j++){
+            triangle[i][j] += Math.max(triangle[i - 1][j - 1] || 0, triangle[i - 1][j] || 0);
+        };
+    };
+    return Math.max(...triangle[triangle.length - 1]);
+}
